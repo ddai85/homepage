c1fa77a6e48483c52a90c0c81a48792911c68b5f
DanDai <ddai85@gmail.com>
2017-11-21 19:44:07 -0800

    Initialize webpage with boilerplate React code

---
+++ client/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>hello world!!!</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
